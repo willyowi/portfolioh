568d14e723959118326a8bf8eca23c417b73f071
willyowi <wilsonowino1@gmail.com>
2019-10-07 17:24:15 +0300

    portfolio

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// $(function() {
+
+//     $('[data-toggle="moda"]').click(function() {
+//       var modalId = $(this).data('targe');
+//       $(modalId).modal('show');
+  
+//     });
+
+//     $('[data-toggle="two"]').click(function() {
+//       var modalId = $(this).data('target');
+//       $(modalId).modal('show');
+  
+//     });
+  
+//   });
